8448a662a7d4dbb2635dcfe60cf51417c690dc18
kkpncc2023@gmail.com <kkpncc2023@gmail.com>
2026-05-30 01:45:11 +0530

    changes again in db and json file

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 // const mongoURL = process.env.DB_URL_LOCAL 
 const mongoURL = process.env.DB_URL;
 // connect to the MongoDB database using Mongoose
-mongoose.connect(mongoURL);
+mongoose.connect(process.env.DB_URL);
 
 // get the default connection
 const db = mongoose.connection;
